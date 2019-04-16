32502fb76123548cab654c5163e29bec4ea0bfef
federamirez00 <20140050@ricaldone.edu.sv>
2019-04-16 12:56:10 -0600

    diseño navbar

--- mocheros/core/helpers/navbar_public.php
+++ mocheros/core/helpers/navbar_public.php
@@ -1,52 +1,25 @@
-<!--Estructura del navbar
-                <div class="navbar-fixed">
-                    <nav class="grey lighten-5">
-                        <div class="nav-wrapper">
-                            <a href="index.php" class="brand-logo logok"> <img src="../web/images/logo.png " height="50"></a>
-                            <a href="#" data-activates="mobile-demo" class="button-collapse cyan-text"><i class="material-icons ">menu</i></a>
-                            <ul class="right hide-on-med-and-down">
-                            <li><a class="cyan-text darken-2" href="inicio.php">Inicio</a></li>
-                            <li><a class="cyan-text darken-2" href="quienes_somos.php"> Nosotros</a></li>
-                            <li><a class="cyan-text darken-2" href="productos.php"> Productos</a></li>
-                            <li><a class="cyan-text darken-2" href="promociones.php"> Promociones</a></li>
-                            <li><a class="cyan-text darken-2" href="faq.php"> FAQ</a></li>
-                            <li><a class="cyan-text darken-2" href="carrito.php"> Carrito</a></li>
-                            <li><a class="cyan-text darken-2" href="index.php"> Cerrar sesión</a></li>
-                            
-                            </ul>
-                        </div> 
-                    </nav>
-                </div> 
-                <!-Estructura del navbar para dispositivos moviles->
-                <ul class="side-nav " id="mobile-demo">
-                        <li><a class="cyan-text darken-2" href="inicio.php">Inicio</a></li>
-                        <li><a class="cyan-text darken-2" href="quienes_somos.php"> Nosotros</a></li>
-                        <li><a class="cyan-text darken-2" href="productos.php"> Productos</a></li>
-                        <li><a class="cyan-text darken-2" href="promociones.php"> Promociones</a></li>
-                        <li><a class="cyan-text darken-2" href="faq.php"> FAQ</a></li>
-                        <li><a class="cyan-text darken-2" href="carrito.php"> Carrito</a></li>
-                        <li><a class="cyan-text darken-2" href="index.php"> Cerrar sesión</a></li>
-                </ul>-->
-<nav class="amber accent-3">
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo"></a>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
-            <li><a href="mochilas.php"><i class="material-icons left">work</i>Mochilas</a></li>
-            <li><a href="loncheras.php"><i class="material-icons left">work</i>Loncheras</a></li>
-            <li><a href="accesorios.php"><i class="material-icons left">watch</i>Accesorios</a></li>
-            <li><a href="carrito.php"><i class="material-icons left">shopping_cart</i>Carrito</a></li>
-            <li><a class="modal-trigger" href="#modal1"><i class="material-icons left">person</i>Iniciar sesión</a></li>
-            <li><a class="modal-trigger" href="#modal2"><i class="material-icons left">person_add</i>Registrarse</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-<ul class="sidenav" id="mobile-demo">
+<!--Estructura del navbar-->
+<div class="navbar-fixed">
+    <nav class="amber accent-3">
+        <div class="nav-wrapper">
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+                <li><a href="mochilas.php"><i class="material-icons left">work</i>Mochilas</a></li>
+                <li><a href="loncheras.php"><i class="material-icons left">work</i>Loncheras</a></li>
+                <li><a href="accesorios.php"><i class="material-icons left">watch</i>Accesorios</a></li>
+                <li><a href="carrito.php"><i class="material-icons left">shopping_cart</i>Carrito</a></li>
+                <li><a class="modal-trigger" href="#modal1"><i class="material-icons left">person</i>Iniciar sesión</a></li>
+                <li><a class="modal-trigger" href="#modal2"><i class="material-icons left">person_add</i>Registrarse</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<ul class="side-nav" id="mobile-demo">
     <li><a href="index.php">Mocheros</a></li>
     <hr>
-    <li><a href="accesorios.php">Inicio</a></li>
+    <li><a href="index.php">Inicio</a></li>
     <li><a href="mochilas.php">Mochilas</a></li>
     <li><a href="loncheras.php">Loncheras</a></li>
     <li><a href="accesorios.php">Accesorios</a></li>
