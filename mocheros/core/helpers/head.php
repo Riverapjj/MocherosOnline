cdc6bbb57ca5ae0d45e2622c9e17ac5b0deb9e0f
Josué Rivera <keko.rai.jr@gmai.com>
2019-04-16 20:54:42 -0600

    uniendo helpers del sitio publido remastered

---
+++ mocheros/core/helpers/head.php
@@ -0,0 +1,13 @@
+<!--Importando los iconos de google materialize-->
+<link href="../../resources/css/material_icons.css" rel="stylesheet">
+<meta charset="utf-8">
+<!--importando el css materialize.css-->
+<link type="text/css" rel="stylesheet" href="../../resources/css/materialize.min.css" media="screen,projection" />
+<!-- Importando el style.css -->
+<link type="text/css" rel="stylesheet" href="../../resources/css/style.css" media="screen,projection" />
+<link rel="stylesheet" href="../../resources/css/animate.css">
+<link rel="shortcut icon" href="../../resources/img/mocheros-logo.jpg">
+<!--Importando fontawesome icons-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<!--Le hacemos saber al navegador que esté optimizable para moviles-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
