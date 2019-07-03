234d7323fdf789af2d789f6498683c377c1c411f
Estudiante <fede.forever13@gmail.com>
2019-07-03 11:53:11 -0600

    importar controlador de carrito

--- mocheros/views/public/carrito.php
+++ mocheros/views/public/carrito.php
@@ -4,28 +4,30 @@ publicHelper::header('Carrito de compras');
 ?>
 <main class="grey lighten-2">
     <div class="container">
-        <table class="responsive-table">
-            <thead>
-                <tr>
-                    <th>Foto</th>
-                    <th>Artículo</th>
-                    <th>Cantidad</th>
-                    <th>Precio unitario</th>
-                    <th>Sub Total</th>
-                </tr>
-            </thead>
-            <tbody id="detalleVenta"></tbody>
-        </table>
-    </div>
-    <!--Apartado donde aparece el total a apagar y la opción para pagar-->
-    <div class="container monto">
         <div class="row">
-            <div class="col s12 m6 l12">
-                <div id="total"></div>
+            <div class="col s12 m12 l9">
+                <div class="container-fluid">
+                    <div id="detalle-venta"></div>
+                </div>
+            </div>
+            <div id="total"></div>
+        </div>
+    </div>
+    <div id="modal-comprobante" class="modal">
+        <div class="modal-content">
+            <h4 class="center">Aviso</h4>
+            <div class="container">
+                <form class="col s12" method="post" id="form-create" enctype="multipart/form-data">
+                    <h6>La venta ha sido realizada con éxito. El comprobante de compra será realizado en un momento.</h6>                    
+                    <div class="row center">                     
+                    <a class="waves-effect waves-light btn green" href='../../core/reports/public/comprobante.php'><i class="material-icons">check</i></a>           
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </main>
+<script src="../../core/controllers/public/carrito.js"></script>
 <?php
 publicHelper::footer('carrito.js');
 ?>
